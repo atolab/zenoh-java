e5017b5d2d6e99abb6aa8d67d7a86227ad1aef41
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2019-07-09 10:32:58 +0200

    Add Zenoh.open() with user/passwd

--- zenoh/src/main/java/io/zenoh/Zenoh.java
+++ zenoh/src/main/java/io/zenoh/Zenoh.java
@@ -68,6 +68,31 @@ public class Zenoh {
     }
 
     /**
+     * Open a connection to a Zenoh broker, using a login and password.
+     * @param locator the Zenoh broker's locator.
+     * @param uname the user name.
+     * @param passwd the password.
+     * @return a Zenoh object to be used for requests on the Zenoh broker.
+     * @throws ZException if connection fails.
+     */
+    public static Zenoh open(String locator, String uname, String passwd) throws ZException {
+        LOG.debug("Call z_open_wup on {}", locator);
+        z_zenoh_p_result_t zenoh_result = zenohc.z_open_wup(locator, uname, passwd);
+        if (zenoh_result.getTag().equals(result_kind.Z_ERROR_TAG)) {
+            throw new ZException("z_open on " + locator + " failed", zenoh_result.getValue().getError());
+        }
+        SWIGTYPE_p_z_zenoh_t z = zenoh_result.getValue().getZenoh();
+
+        LOG.debug("Call z_start_recv_loop");
+        int loop_result = zenohc.z_start_recv_loop(z);
+        if (loop_result != 0) {
+            throw new ZException("z_start_recv_loop failed", loop_result);
+        }
+
+        return new Zenoh(z);
+    }
+
+    /**
      * Declares a Publisher on a resource
      * @param resource the resource published by the Publisher
      * @return the Publisher to be used for publications.
