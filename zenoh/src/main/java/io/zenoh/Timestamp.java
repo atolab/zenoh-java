ed5bd48219249f4353b301ad055f1a5794ae4d75
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2019-11-06 12:27:37 +0100

    Improve Timestamp (Comparable, equals(), toString(), getTimeAsInstant())

--- zenoh/src/main/java/io/zenoh/Timestamp.java
+++ zenoh/src/main/java/io/zenoh/Timestamp.java
@@ -1,16 +1,21 @@
 package io.zenoh;
 
+import java.time.Instant;
+import java.util.Arrays;
+
 /**
  * A Zenoh timestamp.
  */
-public class Timestamp {
+public class Timestamp implements Comparable<Timestamp> {
 
     private long time;
     private byte[] clockId;
+    private String asString;
 
     protected Timestamp(long time, byte[] clockId) {
         this.time = time;
         this.clockId = clockId;
+        this.asString = null;
     }
 
     /**
@@ -26,4 +31,67 @@ public class Timestamp {
     public byte[] getClockId() {
         return clockId;
     }
+
+    /* number of NTP fraction per second (2^32) */
+    private static final long FRAC_PER_SEC = 0x100000000L;
+
+    /* number of nanoseconds per second (10^9) */
+    private static final long NANO_PER_SEC = 1000000000L;
+
+
+    public Instant getTimeAsInstant() {
+        Instant i = Instant.ofEpochSecond(time >> 32);
+        long frac = time & 0xffffffffL;
+        return i.plusNanos((frac * NANO_PER_SEC) / FRAC_PER_SEC);
+    }
+
+    @Override
+    public int compareTo(Timestamp o) {
+        int tcmp = Long.compare(this.time, o.time);
+        if (tcmp != 0) {
+            return tcmp;
+        }
+
+        for (int i = 0; i < this.clockId.length; i++) {
+            int bcmp = Byte.compare(this.clockId[i], o.clockId[i]);
+            if (bcmp != 0) {
+                return bcmp;
+            }
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (this == obj)
+            return true;
+        if (! (obj instanceof Timestamp))
+            return false;
+
+        Timestamp o = (Timestamp) obj;
+        return this.time == o.time && Arrays.equals(this.clockId, o.clockId);
+    }
+
+    private static final char[] HEX_DIGITS = "0123456789abcdef".toCharArray();
+
+    @Override
+    public String toString() {
+        synchronized (this) {
+            if (asString == null) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(getTimeAsInstant().toString())
+                  .append('/');
+                  for (byte b : clockId) {
+                    sb.append(HEX_DIGITS[(b & 0xF0) >> 4])
+                      .append(HEX_DIGITS[b & 0x0F]);
+                }
+                asString = sb.toString();
+            }
+        }
+        return asString;
+    }
+
 }
